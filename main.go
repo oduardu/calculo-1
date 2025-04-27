6e25f8e5bd973e17639eb828798d292a3da1fe1a
Eduardo P Zancanaro <eduardo.zancanaro@estudante.uffs.edu.br>
2025-04-27 18:31:28 -0300

    feat: criado função bisseccao e refatorado código

--- main.go
+++ main.go
@@ -6,23 +6,47 @@ import (
 )
 
 func main() {
-	result1 := eq1(5)
-	result2 := eq1(2)
 
-	if result1+result2 == 0 {
+	fmt.Println("A equação 1 é: x^4 + √x - 2x - 4")
+	fmt.Println("A equação 2 é: 2cos(x) - x + 6")
+	fmt.Println("Selecione qual das equações você deseja verificar se tem solução:")
+	fmt.Println("1 - Equação 1")
+	fmt.Println("2 - Equação 2")
+
+	var opcao int
+	fmt.Scan(&opcao)
+
+	if opcao != 1 && opcao != 2 {
+		fmt.Println("Opção inválida.")
+		return
+	}
+
+	var num1, num2 float64
+	fmt.Println("Digite o primeiro número:")
+	fmt.Scan(&num1)
+	fmt.Println("Digite o segundo número:")
+	fmt.Scan(&num2)
+
+	if sinaisOpostos(num1, num2, opcao) {
 		fmt.Println("A equação tem pelo menos uma solução neste intervalo.")
+		bisseccao(num1, num2, opcao)
 	} else {
 		fmt.Println("Não é possível afirmar que existe solução neste intervalo, tente outros dois números.")
 	}
+}
 
-	result3 := eq2(6)
-	result4 := eq2(9)
+func sinaisOpostos(a, b float64, opcao int) bool {
+	var fa, fb int
 
-	if result3+result4 == 0 {
-		fmt.Println("A equação tem pelo menos uma solução neste intervalo.")
+	if opcao == 1 {
+		fa = eq1(a)
+		fb = eq1(b)
 	} else {
-		fmt.Println("Não é possível afirmar que existe solução neste intervalo, tente outros dois números.")
+		fa = eq2(a)
+		fb = eq2(b)
 	}
+
+	return fa*fb < 0
 }
 
 func eq1(x float64) int {
@@ -43,4 +67,31 @@ func eq2(x float64) int {
 	}
 
 	return 1
+}
+
+func bisseccao(a, b float64, opcao int) {
+	const erro = 1e-1
+	var meio float64
+	var fmeio int
+
+	for (b-a)/2 > erro {
+		meio = (a + b) / 2
+
+		if opcao == 1 {
+			fmeio = eq1(meio)
+		} else {
+			fmeio = eq2(meio)
+		}
+
+		if fmeio == 0 {
+			break
+		} else if sinaisOpostos(a, meio, opcao) {
+			b = meio
+		} else {
+			a = meio
+		}
+	}
+
+	fmt.Printf("A solução aproximada é x = %.5f\n", meio)
+	fmt.Printf("Intervalo final: [%.5f, %.5f]\n", a, b)
 }
